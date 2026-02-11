cebd511eab60e2378e9f79845cc4671da184c369
Evan Czako <evan.czako@gmail.com>
2026-02-10 23:46:27 -0500

    Update backend build script

--- backend/scripts/install-stockfish.sh
+++ backend/scripts/install-stockfish.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-mkdir -p bin
-curl -L https://stockfishchess.org/files/stockfish-ubuntu-x86-64.tar -o sf.tar
-tar -xf sf.tar -C bin
-chmod +x bin/stockfish
+set -e
+
+mkdir -p ../bin
+
+# Download Linux Stockfish zip
+curl -L https://stockfishchess.org/files/stockfish-15.1-linux-x64-bmi2.zip -o sf.zip
+
+# Unzip into bin folder
+unzip -o sf.zip -d ../bin
+
+# Make executable (binary inside the zip is usually named 'stockfish-15.1-linux-x64-bmi2')
+chmod +x ../bin/stockfish-15.1-linux-x64-bmi2
+
+# Optionally, rename to simple 'stockfish' for easier path reference
+mv ../bin/stockfish-15.1-linux-x64-bmi2 ../bin/stockfish
+
+# Clean up
+rm sf.zip
