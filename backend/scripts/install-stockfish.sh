cfeb39905ca5c255720d7290eb780d7afc539b1e
Evan Czako <evan.czako@gmail.com>
2026-02-11 00:00:23 -0500

    This commit is dedicated to the brooklyn baddiez

--- backend/scripts/install-stockfish.sh
+++ backend/scripts/install-stockfish.sh
@@ -4,13 +4,16 @@ set -e
 # Ensure bin folder exists
 mkdir -p ../bin
 
-# Download Stockfish Linux tar (direct GitHub release)
+# Download Stockfish Linux tar
 curl -L -o ../bin/stockfish.tar https://github.com/official-stockfish/Stockfish/releases/download/sf_18/stockfish-ubuntu-x86-64-avx2.tar
 
 # Extract tar into bin folder
 tar -xf ../bin/stockfish.tar -C ../bin
 
-# Find the extracted binary (it may be nested in a folder)
+# Remove old 'stockfish' if it exists
+rm -f ../bin/stockfish
+
+# Find the extracted binary (nested in folder)
 EXTRACTED_BINARY=$(find ../bin -type f -name "stockfish-ubuntu-x86-64-avx2" | head -n 1)
 
 if [ -z "$EXTRACTED_BINARY" ]; then
@@ -21,8 +24,8 @@ fi
 # Make it executable
 chmod +x "$EXTRACTED_BINARY"
 
-# Rename to simple 'stockfish' for consistent backend path
-mv "$EXTRACTED_BINARY" ../bin/stockfish
+# Copy (not move) to ../bin/stockfish
+cp "$EXTRACTED_BINARY" ../bin/stockfish
 
 # Clean up tar
 rm ../bin/stockfish.tar
